38dce09128457399a41a180471023420c40ab9ec
ramiromanoel <ramiro.junior1@gmail.com>
2026-09-18 17:06:07 -0300

    criacao da classe de coleta

--- collect.py
+++ collect.py
@@ -1,4 +1,71 @@
 # %%
-print( "Olá mundo")
+import pandas as pd
+pd.set_option('display.max_columns', None)
+
+import fastf1
+import time
+
+import argparse
 
 # %%
+class CollectResults:
+
+    def __init__(self, years=[2021,2022,2023], modes=["R", "S"]):
+        self.years = years
+        self.modes = modes
+
+
+    def get_data(self, year, gp, mode)-> pd.DataFrame:
+        try:
+            session = fastf1.get_session(year, gp, mode)
+
+        except ValueError as err:
+            return pd.DataFrame()
+
+        session._load_drivers_results()
+
+        df = session.results
+        df["Mode"] = mode
+
+        return session.results
+
+
+    def save_data(self, df, year, gp, mode):
+        df.to_parquet(f"data/{year}_{gp:02}_{mode}.parquet")
+
+
+    def process(self, year, gp, mode):
+        df = self.get_data(year, gp, mode)
+        if df.empty:
+            return False
+        
+        self.save_data(df, year, gp, mode)
+        return True
+
+
+    def process_year_modes(self, year):
+        for i in range(1,50):
+            for mode in self.modes:
+                if not self.process(year, i, mode) and mode == "R":
+                    return
+
+
+    def process_years(self):
+        for year in self.years:
+            print(f"Coletando dados do ano {year}")
+            self.process_year_modes(year)  
+            time.sleep(10)  
+
+
+#%%
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--years", "-y", nargs="+", type=int)
+    parser.add_argument("--modes", "-m", nargs="+")
+
+    args = parser.parse_args()
+
+    collect = CollectResults(args.years, args.modes)
+    collect.process_years()
+
